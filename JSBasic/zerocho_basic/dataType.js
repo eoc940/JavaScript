7bbefc8f2844da603f6ef8ef0f54819285318993
Hyunggeun Kim <hgk0000@naver.com>
2023-04-10 00:07:09 +0900

    feat: dataType study

---
+++ JSBasic/zerocho_basic/dataType.js
@@ -0,0 +1,35 @@
+console.log('안녕하세요\nㅎㅎㅎㅎ')
+console.log("안녕하세요\nㅎㅎㅎㅎ")
+console.log(typeof "dddd")
+console.log(typeof '') // 이 경우도 string 으로 나온다
+console.log("'")
+console.log('"')
+console.log('how\'re you')
+console.log('바나나' + ' 사과')
+console.log(5e4)
+console.log(5e-4)
+console.log(typeof 0x1a1)
+console.log(typeof NaN) // Not a Number 로 타입은 number 이다
+console.log(parseInt('1234'))
+console.log(parseInt('3.14')) // parseInt는 정수로 바꾸는 것
+console.log(parseFloat('3.14'))
+console.log(parseInt('3월')) // 3만 뽑아서 바꿔줌
+console.log('string' + 0) // 문자열과 아닌것이 더해질때 문자열로 변환됨
+console.log(NaN == NaN) // false
+console.log(true > false) // true
+console.log('1' == 1) // 값만 비교
+console.log('1' === 1) // 타입까지 비교
+console.log(!!'a') // !! 은 360도 회전 느낌으로 bool 타입을 보여준다고 생각하자
+
+// undefined 는 컴퓨터는 항상 무언가를 돌려주는데 돌려줄 값이 없을 때 돌려주는 것이라고 생각하자
+// null 은 의도적으로 빈값을 넣을 때 많이 쓴다
+console.log(undefined == null) // true
+console.log(undefined === null) // false
+console.log(typeof null) // object가 뜸
+
+
+
+
+
+
+

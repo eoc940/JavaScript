20a1a2cdbd782f070fb71a501f9acb833cd64de1
Hyunggeun Kim <hgk0000@naver.com>
2023-04-15 02:00:44 +0900

    feat: variable study

---
+++ JSBasic/zerocho_basic/variable.js
@@ -0,0 +1,14 @@
+let empty;
+console.log(typeof empty); // 선언만 된건 undefined
+
+// 변수에 특수문자, 한자 $, _ 사용가능하다
+// 키워드는 변수명으로 못씀
+// 값이 없음을 넣을때는 undefined가 아니라 null을 넣자
+let hello = '123'
+hello = null
+
+// 변수는 let, 상수는 const로 쓰자. var는 쓰지말자
+
+
+
+
